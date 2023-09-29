e2262ff980288b723f23ec7e7c335a4b09640d51
Eva Dee <includejsdev@gmail.com>
2023-09-29 13:20:14 +0000

    feat: configured bucket for hosting and added files #33

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   value       = module.terrahouse_aws.bucket_name
   description = "name of the bucket"
 }
+
+output "S3_bucket_endpoint" {
+  value       = module.terrahouse_aws.bucket_endpoint
+  description = "S3 static website hosting endpoint"
+}
