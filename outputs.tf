c81df1c2f2eaf77e22c2d805024b846a5291275a
Eva Dee <includejsdev@gmail.com>
2023-10-10 11:54:40 +0000

    use custom provider #48

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  value       = module.terrahouse_aws.bucket_name
-  description = "name of the bucket"
-}
+#output "bucket_name" {
+#  value       = module.terrahouse_aws.bucket_name
+#  description = "name of the bucket"
+#}
 
-output "S3_bucket_endpoint" {
-  value       = module.terrahouse_aws.bucket_endpoint
-  description = "S3 static website hosting endpoint"
-}
+#output "S3_bucket_endpoint" {
+#  value       = module.terrahouse_aws.bucket_endpoint
+#  description = "S3 static website hosting endpoint"
+#}
 
-output "cloudfront_url" {
-  value       = module.terrahouse_aws.cloudfront_url
-  description = "CloudFront distribution url"
-}
+#output "cloudfront_url" {
+#  value       = module.terrahouse_aws.cloudfront_url
+#  description = "CloudFront distribution url"
+#}
